85b66b8ab62465aab5249ea47924e4fc77442e05
Gordon Huang <dqna64@gmail.com>
2023-02-27 03:20:12 +1100

    array mutate via pointer

--- containers/arrays.cpp
+++ containers/arrays.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
 #include <array>
 
-void print_arr_address(int *arr, size_t size) {
+void print_arr_address(int *arr, size_t size)
+{
     std::cout << "arr: " << arr << std::endl;
 }
 
-int main(void) {
+void print_array(int *arr, size_t size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void mutate_array(int arr[], size_t size)
+{
+    if (size >= 1)
+    {
+        arr[0] += 1;
+    }
+}
+
+void pass_to_fn_and_mutate()
+{
+    std::array<int, 5> arr = {1, 2, 3, 4, 5};
+    std::cout << "arr before mutate in fn" << std::endl;
+    print_array(arr.data(), arr.size());
+    mutate_array(arr.data(), arr.size());
+    std::cout << "arr after mutate in fn" << std::endl;
+    print_array(arr.data(), arr.size());
+}
+
+void array_front_back()
+{
+    std::array<int, 5> a = {1, 2, 3, 4, 5};
+    std::cout << "array front: " << a.front() << std::endl;
+    std::cout << "array back: " << a.back() << std::endl;
+}
+
+int main(void)
+{
     // Declare an array with type and size.
     std::array<int, 5> a = {1, 2, 3, 4, 5};
     std::array<int, 5> a1;
-    a1 = a; // Copy a into a1 by assignment. Must have the EXACT same size.
+    a1 = a;      // Copy a into a1 by assignment. Must have the EXACT same size.
     auto a2 = a; // Using auto, a2 has inferred type std::array<int, 5>
     auto a3(a);
 
     print_arr_address(a.data(), a.size());
-    
+
+    pass_to_fn_and_mutate();
+
+    array_front_back();
+
     return 0;
 }
